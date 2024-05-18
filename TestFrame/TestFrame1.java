4b673e59993cf3abe1e2252cb78c5fb932424273
jhon-rjf <ykjung0917@kakao.com>
2024-05-18 14:23:36 +0900

    2. write TestFrame.java

---
+++ TestFrame/TestFrame1.java
@@ -0,0 +1,8 @@
+package TestFrame;// TestFrame.java-------------------------------------
+import junit.framework.TestCase;
+public class TestFrame extends TestCase{
+	public TestFrame(String name){
+		super(name);
+	}
+	// public void test???
+}
