2b45e92edbf6267695a51ec15f0679264b700c51
jhon-rjf <ykjung0917@kakao.com>
2024-05-18 15:03:42 +0900

    add method 'add'

--- TestFrame.java
+++ TestFrame.java
@@ -5,8 +5,9 @@ public class TestFrame extends TestCase{
     public TestFrame(String name){
         super(name);
     }
-    public void testScoreNoThrows(){
+    public void testAddOneThrow(){
         Frame f = new Frame();
-        assertEquals(0,f.getScore());
+        f.add(5); // 투구 -> 쓰러뜨린 핀수를 더한다.
+        assertEquals(5, f.getScore()); // score가 5로 제대로 나오는 지 테스트
     }
 }
