6714db5076d4372d5f2890dc2509d91167872644
jhon-rjf <ykjung0917@kakao.com>
2024-05-18 15:40:43 +0900

    edit TestGame.java - testTwoThrowsNoMark

--- TestGame.java
+++ TestGame.java
@@ -4,9 +4,11 @@ public class TestGame extends TestCase {
     public TestGame(String name){
         super(name);
     }
-    public void testOneThrow() {
+    public void testTwoThrowsNoMark()
+    {
         Game g = new Game();
         g.add(5);
-        assertEquals(5,g.score());
+        g.add(4);
+        assertEquals(9, g.score());
     }
 }
