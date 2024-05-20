45d8ad2628f2cff5bd1b47eb094e6f383de92e9d
jhon-rjf <ykjung0917@kakao.com>
2024-05-20 23:51:43 +0900

    set private ~~~

--- Game.java
+++ Game.java
@@ -14,13 +14,13 @@ public class Game {
     }
 
     void adjustCurrentFrame(int pins) {
-        if (firstThrow == true) {
+        if (firstThrowInFrame == true) {
             if(pins==10)//스트라이크
                 itsCurrentFrame++;
             else
-                firstThrow = false;
+                firstThrowInFrame = false;
         } else {
-            firstThrow=true;
+            firstThrowInFrame=true;
             itsCurrentFrame++;
             // 내가 던지고 있는 프레임 번호를 지정하기 위해 각 프레임의 마지막 투구 직후에 증가
             // 다음에 투구할 프레임을 가르키게 됨
@@ -30,15 +30,15 @@ public class Game {
 
     public int scoreForFrame(int theFrame)
     {
-        int ball = 0;
+        ball = 0;
         int score = 0;
 
         for (int currentFrame = 0; currentFrame < theFrame;	currentFrame++) {
-            int firstThrow = itsThrows[ball++]; // 첫 번째 투구 계산
+            firstThrow = itsThrows[ball++]; // 첫 번째 투구 계산
             if (firstThrow == 10) {
                 score += 10 + itsThrows[ball] + itsThrows[ball + 1];
             } else {
-                int secondThrow = itsThrows[ball++];
+                 secondThrow = itsThrows[ball++];
 
                 int frameScore = firstThrow + secondThrow;
                 if (frameScore == 10)
@@ -62,6 +62,9 @@ public class Game {
         // 현재 프레임인 itsCurrentFrame을 반환
     }
 
+    private int ball;
+    private int firstThrow;
+    private int secondThrow;
 
 
 
@@ -69,6 +72,6 @@ public class Game {
     private int[] itsThrows = new int[21]; // 프레임
     private int itsCurrentThrow = 0;// 투구 순번
     private int itsCurrentFrame = 1;; // 현재 프레임 번호
-    private boolean firstThrow = true; // 각 프레임에서 첫번째 투구 여부
+    private boolean firstThrowInFrame = true; // 각 프레임에서 첫번째 투구 여부
 
 }
